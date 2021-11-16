e09f45b5d8d05bfa4e7bff7943cb8e52a57c9c2e
Sidikov Marsel <sidikov.marsel@gmail.com>
2021-11-16 20:24:49 +0300

    add 21 - add inheritance

---
+++ Projects/16. Inheritance and Polymorphism/src/example1/Ellipse.java
@@ -0,0 +1,12 @@
+package example1;
+
+/**
+ * 16.11.2021
+ * 16. Inheritance and Polymorphism
+ *
+ * @author Sidikov Marsel (First Software Engineering Platform)
+ * @version v1.0
+ */
+// овал
+public class Ellipse {
+}
